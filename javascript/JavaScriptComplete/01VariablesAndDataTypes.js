332937bc116f0ee83e76f0b91de5399e253dee44
sarveshhome <sarvesh.carlton@gmail.com>
2019-09-28 23:12:45 +0530

    Variable Basic Operators

---
+++ javascript/JavaScriptComplete/01VariablesAndDataTypes.js
@@ -0,0 +1,43 @@
+var firstname = "sarvesh";
+console.log(firstname);
+
+var lastname ="kumar";
+var age =26;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "ITProfessional";
+console.log(job);
+
+//var 2years = 2; //Error  Invalid or unexpected token
+var $2years = 2 ;
+var _2years = 2;
+//var &2years =2; //Unexpected token &
+
+/**
+ * we can not special characters in variable
+ */
+//var 2yea&rs=2; //Invalid or unexpected token
+var years = 2;
+
+//var sarvesh/kumar ="sarvesh and kumar";  
+// Unexpected token 
+
+var sarveshkumar ="sarvesh and kumar";
+
+//var function = 27;
+/**
+ * reserve keyword not used as a variable in javascript 
+ */
+//var delete = 27   //Unexpected token delete
+
+//var if = 27 ;       //Unexpected token if
+
+
+
+
+
---
+++ javascript/javascriptbeginner/04Class.js
@@ -0,0 +1,12 @@
+var firstname = "sarvesh";
+console.log(firstname);
+
+var lastname ="kumar";
+var age =26;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
