3d0708ecfb3e259f61086c9128f37f1b85200daf
sarveshhome <sarvesh.carlton@gmail.com>
2019-09-30 06:00:26 +0530

    Operator precedence

--- javascript/JavaScriptComplete/04OperatorPrecedence.js
+++ javascript/JavaScriptComplete/04OperatorPrecedence.js
@@ -1,9 +1,37 @@
 /***********************************
  * Operator precedence
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
  */
 var now = 2019;
 var yearSarvesh = 1985;
 var fullAge = 18;
 
+//Multiple operator
 var isFullAge = now - yearSarvesh >= fullAge ;
 console.log(isFullAge);
+
+//Grouping
+var ageSarvesh = now - yearSarvesh;
+var ageTarun = 37;
+var average = (ageSarvesh + ageTarun) /2;
+
+console.log(average);
+
+//Multiple assignments
+var x,y;
+x = y = (3 + 5) * 4 - 6;
+
+console.log(x,y);
+
+//More operators
+x = x *2;
+//x*=2;
+console.log('x multiple : '+x);
+
+x = x +1;
+x+=1;
+x++;
+console.log('Assignments right  to left : '+ x);
+
+x--;
+console.log('minus operator right to left : '+ x);
