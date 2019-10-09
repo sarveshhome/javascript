90a3c9ea31ba142c4b093b3aed4cd8eee4075e99
sarveshhome <sarvesh.carlton@gmail.com>
2019-10-09 21:53:38 +0530

    Functions

--- javascript/JavaScriptComplete/09Functions.js
+++ javascript/JavaScriptComplete/09Functions.js
@@ -3,5 +3,23 @@
  */
 
  function calculateAge(birthday){
-
+     return 2019 - birthday;
  }
+
+ var ageSarvesh=  calculateAge(1989);
+ var ageShashi = calculateAge(1994);
+ var ageTarun = calculateAge(1996);
+
+  console.log(ageSarvesh, ageShashi, ageTarun);
+
+  function yearUntilRetirement(year,firstName){
+      var age = calculateAge(year);
+      var retirement = 65 - age;
+      console.log(firstName + ' retires in ' + retirement +' years.');
+  }
+
+
+  yearUntilRetirement(1989);
+  yearUntilRetirement(1994);
+  yearUntilRetirement(1996);
+  
