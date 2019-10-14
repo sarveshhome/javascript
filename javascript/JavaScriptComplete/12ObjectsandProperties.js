451012d1a297f83e7aad858b5ab18621589a0451
sarveshhome <sarvesh.carlton@gmail.com>
2019-10-14 05:45:34 +0530

    ObjectsandProperties

--- javascript/JavaScriptComplete/12ObjectsandProperties.js
+++ javascript/JavaScriptComplete/12ObjectsandProperties.js
@@ -2,5 +2,25 @@
  * Objects and Properties
  **************************/
 
+//Object literal
+ var sarvesh = {
+        //key:value
+        firstName: "Sarvesh",
+        lastName : "kumar",
+        birthYear :1995,
+        family:['kumar','singh','ram','shyam'],
+        job : 'ITProf',
+        isMarried : false
+ };
 
- 
+ console.log(sarvesh.firstName);
+ console.log(sarvesh['lastName']);
+ var x = 'birthYear';
+ console.log(sarvesh[x]);
+
+ //new Object syntax
+ var kumar = new Object();
+ kumar.firstName ='Sana';
+ kumar['lastName'] ='singh';
+ kumar.birthYear =1991;
+ console.log(kumar);
