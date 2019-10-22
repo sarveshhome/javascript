018f04ced43b2129585d3263fa16af0d0ce0f7d7
sarveshhome <sarvesh.carlton@gmail.com>
2019-10-22 08:18:22 +0530

    FirstClassFunctionsFunctionsReturningFunctions

---
+++ javascript/JavaScriptComplete/25FirstClassFunctionsFunctionsReturningFunctions.js
@@ -0,0 +1,37 @@
+/******************************************************
+ * First Class Functions Functions Returning Functions
+ *****************************************************/
+
+
+function interviewQuestion(job)
+{
+    if (job == 'ITProf') {
+        return function(name){
+            console.log(name + ' , what is object and function in javascript? ');
+        };
+    }
+    else if(job == 'designer'){
+        return function(name)
+        {
+            console.log(name + ' , can you explain what is UI ');
+        };
+    }
+    else{
+       return function(name){
+           console.log('hello ' + name + ' what do you do ?');            
+       };
+    }
+
+}
+
+var ITProfQuestion = interviewQuestion('ITProf');
+
+ITProfQuestion('sarvesh');
+
+var ITProfQuestion = interviewQuestion('designer');
+ITProfQuestion('rahul');
+
+ITProfQuestion('tarun');
+ITProfQuestion('shashi');
+
+interviewQuestion('Tester')('ada lovelace');
