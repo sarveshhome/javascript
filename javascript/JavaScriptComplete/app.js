21019fb63c880ef08ff38586bebb807bba376295
sarveshhome <sarvesh.carlton@gmail.com>
2019-11-01 06:28:38 +0530

    the First Event Listeners

--- javascript/JavaScriptComplete/app.js
+++ javascript/JavaScriptComplete/app.js
@@ -1,28 +1,36 @@
+//Budget Controller
 var budgetController = (function(){
-    var x = 23;
-    var add = function(a){
-        return x +a;        
-    };
-    return {
-        publicTest :function (b)
-        {
-            return add(b);
-        } 
-    };
+    
+
 })();
 
-
+//UI Controller
 var UIController = (function(){
         //some code;
 });
 
+//Global APP Controller
 var controller = (function(budgetCtrl,UICtrl){
-    var z = budgetCtrl.publicTest(5);
+    
+    document.querySelector('.add__btn').addEventListener('click',function(){
+        console.log('button click');
+        //1. Get the field input data
 
-    return {
-        anotherPublic : function(){
-            console.log(z);
-        }
-    };
+        //2.  Add the item to the budget controller
+
+        //3. Add the item to the UI
+
+        //4. Calculate the budget 
+
+        //5. Display the budget on the UI
+
+
+    });
+    //https://developer.mozilla.org/en-US/docs/Web/Events
+    //Keyboard events
+    //http://keycodes.atjayjo.com/#charcode
+    document.addEventListener('keypress',function(event){
+        console.log(event);
+    });
 
 })(budgetController,UIController);
