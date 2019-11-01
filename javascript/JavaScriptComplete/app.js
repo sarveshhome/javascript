9568493b53063c1b48ef6a888c04a03f34b3a11d
sarveshhome <sarvesh.carlton@gmail.com>
2019-11-01 08:37:33 +0530

    ReadingInput Data

--- javascript/JavaScriptComplete/app.js
+++ javascript/JavaScriptComplete/app.js
@@ -6,7 +6,20 @@ var budgetController = (function(){
 
 //UI Controller
 var UIController = (function(){
-        //some code;
+        return {
+            getInputs : function(){
+                return {
+                        type :document.querySelector('.add__type').value, //inc and exp
+                        description : document.querySelector('.add__description').value, //description
+                        value  :parseFloat(document.querySelector('.add__value').value) //value
+                };
+                // var type =document.querySelector('.add__type').value; //inc and exp
+                // var description = document.querySelector('.add__description').value ; //description
+                // var value  = document.querySelector('.add__value').value; //value
+
+            }
+        };
+        
 });
 
 //Global APP Controller
@@ -14,7 +27,8 @@ var controller = (function(budgetCtrl,UICtrl){
 
     var ctrlAddItem = function(){
         //1. Get the field input data
-
+        var input1 = UICtrl
+        console.log(input1);
         //2.  Add the item to the budget controller
 
         //3. Add the item to the UI
@@ -28,7 +42,7 @@ var controller = (function(budgetCtrl,UICtrl){
     document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);    
 
     document.addEventListener('keypress',function(event){
-        console.log(event);
+        //console.log(event);
         if (event.keyCode ===13 || event.which === 13) {
             ctrlAddItem();
         }
