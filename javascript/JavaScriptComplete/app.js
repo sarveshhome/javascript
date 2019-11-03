9cf742dd6911fa1d3cb69c80ad0e3f26309fe510
sarveshhome <sarvesh.carlton@gmail.com>
2019-11-03 13:04:17 +0530

    Reading Input Data final

--- javascript/JavaScriptComplete/app.js
+++ javascript/JavaScriptComplete/app.js
@@ -6,28 +6,44 @@ var budgetController = (function(){
 
 //UI Controller
 var UIController = (function(){
+
+        var DOMstring = {
+            inputType :'.add__type',
+            inputDescription : '.add__description',
+            inputValue : '.add__value',
+            inputBtn : '.add__btn'
+        }
+
         return {
             getInputs : function(){
                 return {
-                        type :document.querySelector('.add__type').value, //inc and exp
-                        description : document.querySelector('.add__description').value, //description
-                        value  :parseFloat(document.querySelector('.add__value').value) //value
+                        type :document.querySelector(DOMstring.inputType).value, //inc and exp
+                        description : document.querySelector(DOMstring.inputDescription).value, //description
+                        value  :parseFloat(document.querySelector(DOMstring.inputValue).value) //value
                 };
                 // var type =document.querySelector('.add__type').value; //inc and exp
                 // var description = document.querySelector('.add__description').value ; //description
                 // var value  = document.querySelector('.add__value').value; //value
 
+            },
+            getDOMString : function(){
+                return DOMstring;
             }
         };
         
-});
+})();
+
+
 
 //Global APP Controller
 var controller = (function(budgetCtrl,UICtrl){
 
+    var DOM = UICtrl.getDOMString();
+
+
     var ctrlAddItem = function(){
         //1. Get the field input data
-        var input1 = UICtrl
+        var input1 = UICtrl.getInputs();
         console.log(input1);
         //2.  Add the item to the budget controller
 
@@ -36,10 +52,10 @@ var controller = (function(budgetCtrl,UICtrl){
         //4. Calculate the budget 
 
         //5. Display the budget on the UI
-        console.log('Add Item Function Work');
-    };
+        //console.log('Add Item Function Work');
+    }
     
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);    
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);    
 
     document.addEventListener('keypress',function(event){
         //console.log(event);
@@ -56,5 +72,5 @@ var controller = (function(budgetCtrl,UICtrl){
 
 
 //https://developer.mozilla.org/en-US/docs/Web/Events
-    //Keyboard events
-    //http://keycodes.atjayjo.com/#charcode
+//Keyboard events
+//http://keycodes.atjayjo.com/#charcode
