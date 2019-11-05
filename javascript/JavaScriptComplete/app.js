b1a1971d42e904e0dc362eb02d2d4ed710fa9a30
sarveshhome <sarvesh.carlton@gmail.com>
2019-11-05 06:29:06 +0530

    ClearingOurInputFields

--- javascript/JavaScriptComplete/app.js
+++ javascript/JavaScriptComplete/app.js
@@ -132,7 +132,19 @@ var UIController = (function(){
                 // Insert the HTML into the DOM
                 document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
             },
-           
+            clearFields: function(){
+                var fields,fieldsArr;
+
+                fields =  document.querySelectorAll(DOMstring.inputDescription +"," + DOMstring.inputValue);
+                fieldsArr = Array.prototype.slice.call(fields);
+
+                fieldsArr.forEach((current,index,array)=> {
+                    current.value ="";
+                });
+
+                fieldsArr[0].focus();
+
+            },
             getDOMString : function(){
                 return DOMstring;
             }
@@ -173,9 +185,12 @@ var controller = (function(budgetCtrl,UICtrl){
 
         //3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
-        //4. Calculate the budget 
+        //4. Clear field
+        UICtrl.clearFields();
 
-        //5. Display the budget on the UI
+        //5. Calculate the budget 
+
+        //6. Display the budget on the UI
         //console.log('Add Item Function Work');
     }
     
