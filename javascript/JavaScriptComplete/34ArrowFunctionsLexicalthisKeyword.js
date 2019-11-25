73fbd45ff6bf3ad7a75df02b2f27df940ce9cabc
sarveshhome <sarvesh.carlton@gmail.com>
2019-11-25 06:45:47 +0530

    Arrow function lexical this

--- javascript/JavaScriptComplete/34ArrowFunctionsLexicalthisKeyword.js
+++ javascript/JavaScriptComplete/34ArrowFunctionsLexicalthisKeyword.js
@@ -18,4 +18,68 @@
     }
 }
 
-box5.clickMe();
+//box5.clickMe();
+
+//ES6
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {     
+
+       
+       document.querySelector('.green').addEventListener('click', ()=> 
+       {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+
+box6.clickMe();
+
+
+//ES6
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: ()=> {
+       document.querySelector('.green').addEventListener('click', ()=> 
+       {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+//box66.clickMe();
+/**
+ * output undefined
+ */
+
+
+function Person(name)
+{
+    this.name = name;
+}
+
+//ES5
+Person.prototype.myFriends5 = function (friends){
+        var arr = friends.map(function(el){
+            return this.name + ' is friends with ' + el;
+        }.bind(this));
+
+        console.log(arr);
+}
+
+var friends = ['sarveh','tarun','shashi'];
+new Person('Sarvesh').myFriends5(friends);
+
+//ES6
+Person.prototype.myFriends6 = function (friends){
+    var arr = friends.map(el=>
+          `${this.name} is friends with ${el}`
+    );
+    console.log(arr);
+}
+
+new Person('Singh').myFriends6(friends);
+
