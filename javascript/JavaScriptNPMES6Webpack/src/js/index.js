0659ebd24d9d408a0ac848421ed6e7cfae73ea08
sarveshhome <sarvesh.carlton@gmail.com>
2019-12-26 06:48:02 +0530

    ES6ModulesWork

---
+++ javascript/JavaScriptNPMES6Webpack/src/js/index.js
@@ -0,0 +1,8 @@
+import str from './modules/Search';  
+//import {add,multiply, ID} from './views/searchView';
+//import {add as a,multiply as m, ID} from './views/searchView';
+import * as SearchView from './views/searchView';
+//console.log(`Using import function ${add(ID,2)} and ${multiply(3,7)} and ${str} `);
+//console.log(`Using import function ${a(ID,2)} and ${m(3,7)} and ${str} `);
+console.log(`Using import function ${SearchView.add(ID,2)} and ${SearchView.multiply(3,7)} and ${str} `);
+
