2c085db96d7cc5d508aacb17b4c952655b349d6a
sarveshhome <sarvesh.carlton@gmail.com>
2020-01-10 07:24:43 +0530

    this

---
+++ javascript/JavaScriptComplete/17ThethisKeyword.js
@@ -0,0 +1,16 @@
+/*
+Excution Context Object
+-----------------------
+ Variable Object(VO)
+------------------------
+Scope Chain
+-----------------------
+"This" variable
+
+
+-> Regular function call :  the this keyword point at the global object,(the window object, in the brwoser)
+
+-> Method call : the this variable points to the object that is calling the method
+
+ The this keyword not assigned a value until a function where it is defined is actually called. 
+*/
