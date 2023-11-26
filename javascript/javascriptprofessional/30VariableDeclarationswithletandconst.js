48dcb124d48dbe566a9929ae336f2eea23633baf
Sarvesh kumar <sarvesh.carlton@gmail.com>
2023-11-26 04:11:37 +0000

    let and const

---
+++ javascript/javascriptprofessional/30VariableDeclarationswithletandconst.js
@@ -0,0 +1,12 @@
+//ES6
+const name6 ='Sarvesh Kumar';
+let age6 = 27;
+//name6 = 'Sarvesh singh'; /**Assignment to constant variable.*/
+console.log(name6);
+
+age6 =33;
+
+console.table([name6,age6])
+
+//Block meaning  {}
+
